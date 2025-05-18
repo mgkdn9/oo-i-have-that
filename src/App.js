da0438e6fe2b85f83a7a74b6622d5e418c784061
Mike Kohlberg <mkohlberg95@gmail.com>
2025-05-18 12:28:36 -0600

    stripped down

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Reactsdfasfas
         </a>
       </header>
     </div>
